cac2e851cecba20d899df9ceed01e87537944223
prybrt06 <priyabrat@MacBook-Air-2.local>
2023-09-04 02:00:29 +0530

    get user by id implemented

--- controllers/getUser.ts
+++ controllers/getUser.ts
@@ -1,5 +1,13 @@
 import User from "../models/user";
 
+export const getOneUser = async (req, res, next) => {
+	const id = req.params.id;
+	const user = await User.findOne({ _id: id });
+
+	res.status(201);
+	res.json({ user: user });
+};
+
 export const getAllUser = async (req, res, next) => {
 	const users = await User.find();
 
--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createUser } from "../controllers/createUser";
-import { getAllUser } from "../controllers/getUser";
+import { getAllUser, getOneUser } from "../controllers/getUser";
 
 export const userRoute = Router();
 
 userRoute.post("/", createUser);
+userRoute.get("/:id", getOneUser);
 userRoute.get("/", getAllUser);
