f5024250add5f3063e6c7f9fbd740d3ef0c3d63e
prybrt06 <priyabrat@192.168.215.33>
2023-09-05 15:24:28 +0530

    User signin implemented

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { createUser } from "../controllers/userController/createUser";
 import { deleteOneUser } from "../controllers/userController/deleteUser";
 import { getOneUser, getAllUser } from "../controllers/userController/getUser";
+import { signin } from "../controllers/userController/signInUser";
 import { updateUser } from "../controllers/userController/updateUser";
 
 export const userRoute = Router();
 
 userRoute.post("/", createUser);
+
+userRoute.post("/signin", signin);
 
 userRoute.get("/:id", getOneUser);
 userRoute.get("/", getAllUser);
